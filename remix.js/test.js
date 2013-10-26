eedf0c278321834ae2830111322f7434c50b7135
crhino <piraino.chris@gmail.com>
2013-10-26 01:16:10 -0400

    Made JSON Object of game_beats with bpm and loudness of each beat.

--- remix.js/test.js
+++ remix.js/test.js
@@ -18,6 +18,7 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey},
           var bpm;
           var bs = [];
           var volume = [];
+          var game_beats = []; // JSON Object for iterating through in the game.
           for(var i = 0; i < track.analysis.beats.length; i++) {
             bpm = 60/parseFloat(track.analysis.beats[i].duration);
             console.log(track.analysis.beats[i]);
@@ -26,6 +27,8 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey},
             //console.log(bpm);
             bs.push(bpm);
             volume.push(parseFloat(track.analysis.beats[i].oseg.loudness_max));
+            var loud = parseFloat(track.analysis.beats[i].oseg.loudness_max)
+            game_beats.push({"bpm": bpm, "loudness": loud});
             //console.log(track.analysis.beats[i].oseg.loudness_start);
             //console.log('next');
           }
@@ -38,6 +41,7 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey},
           console.log(bs.sort());
           console.log(volume.shift(), volume.pop());
           console.log(bs.shift(), bs.pop());
+          console.log(game_beats);
           console.log("Min volume: " + Math.min.apply(null, volume));
           console.log("Max volume: " + Math.max.apply(null, volume));
         } 
