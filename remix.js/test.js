0d942807834cf8bc2921e91a1efc824d7cf2324f
Benjamin Mesirow <benmesirow@wr-130-64-195-36.medford.tufts.edu>
2013-10-26 01:40:42 -0400

    Nothing, really

--- remix.js/test.js
+++ remix.js/test.js
@@ -1,9 +1,9 @@
 var apiKey ='1DXWYOVVQHNF7AR18';
-var trackURL = 'http://localhost:8000/Save The World.mp3';
-var trackID = 'SOJARUS1338A5D5F1C';
+var trackURL = 'http://localhost:8000/Red_Oyster_Cult.mp3';
+//var trackID = 'SOJARUS1338A5D5F1C';
 var context = new webkitAudioContext();
-var _title = 'Save The World';
-var _artist = 'Swedish House Mafia';
+var _title = 'Red Oyster Cult';
+var _artist = 'Guster';
 var url = 'http://developer.echonest.com/api/v4/song/search?format=json&bucket=audio_summary';
 $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey}, 
   function(data) {
@@ -20,7 +20,7 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey},
           var volume = [];
           for(var i = 0; i < track.analysis.beats.length; i++) {
             bpm = 60/parseFloat(track.analysis.beats[i].duration);
-            console.log(track.analysis.beats[i]);
+            //console.log(track.analysis.beats[i]);
             //console.log("next");
             //console.log(track.analysis.beats[i].time);
             //console.log(bpm);
