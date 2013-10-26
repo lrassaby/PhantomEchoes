82490880a5ef409ec9a3d7272d750f76729e6106
crhino <piraino.chris@gmail.com>
2013-10-26 01:00:38 -0400

    Added Echo Nest API html and js files

---
+++ remix.js/test.js
@@ -0,0 +1,45 @@
+var apiKey ='1DXWYOVVQHNF7AR18';
+var trackURL = 'http://localhost:8000/Save The World.mp3';
+var trackID = 'SOJARUS1338A5D5F1C';
+var context = new webkitAudioContext();
+var _title = 'Save The World';
+var _artist = 'Swedish House Mafia';
+var url = 'http://developer.echonest.com/api/v4/song/search?format=json&bucket=audio_summary';
+$.getJSON(url, {title:_title, artist:_artist, api_key:apiKey}, 
+  function(data) {
+    var analysisURL = data.response.songs[0].audio_summary.analysis_url;
+    console.log(analysisURL);
+    var remixer = createJRemixer(context, $, apiKey);
+    remixer.remixTrackByURL(analysisURL, trackURL,
+      function (track, percent) {
+        console.log(percent);
+        console.log(track.status);
+        if(track.status == 'ok') {
+          var bpm;
+          var bs = [];
+          var volume = [];
+          for(var i = 0; i < track.analysis.beats.length; i++) {
+            bpm = 60/parseFloat(track.analysis.beats[i].duration);
+            console.log(track.analysis.beats[i]);
+            //console.log("next");
+            //console.log(track.analysis.beats[i].time);
+            //console.log(bpm);
+            bs.push(bpm);
+            volume.push(parseFloat(track.analysis.beats[i].oseg.loudness_max));
+            //console.log(track.analysis.beats[i].oseg.loudness_start);
+            //console.log('next');
+          }
+          console.log(bs);
+          console.log(volume);
+          bs.sort();
+          //var v = volume.copy();
+          //var b = bs.copy();
+          console.log(volume.sort());
+          console.log(bs.sort());
+          console.log(volume.shift(), volume.pop());
+          console.log(bs.shift(), bs.pop());
+          console.log("Min volume: " + Math.min.apply(null, volume));
+          console.log("Max volume: " + Math.max.apply(null, volume));
+        } 
+    });
+});
