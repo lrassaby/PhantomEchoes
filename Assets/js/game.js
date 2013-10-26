1469f855f6f091f6c50b4238453b23587094d92d
Brook <brookelfnichols@gmail.com>
2013-10-26 03:05:31 -0400

    rearranged files

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -1,5 +1,9 @@
 score = 0;
 levelnumber = 1;
+moveup = false;
+movedown = false;
+moveright = false;
+moveleft = false;
 items = {
 	obstacles: [],
 	avatar: null
@@ -17,29 +21,31 @@ function Item (img, sx, sy, swidth, sheight, x, y, width, height, speed) {
 	this.height = height;
 	this.speed = speed;
 
+
 	this.breathefire = function () {
 		alert("breathing fire");
 	}
-	
-	this.moveup = function () {
-		alert("moving up");
-	}
-	
-	this.movedown = function () {
-		alert("moving down");
-	}
-	
-	this.moveleft = function () {
-		alert("moving left");
-	}
 		
-	this.moveright = function () {
-		alert("moving right");
-	}
-	
 	this.drawme = function () {
+      
+        if (moveup){
+            this.y -= this.speed;           
+        }
+        if(movedown){
+            this.y += this.speed;
+        }
+        if(moveleft){
+            this.x -= this.speed;
+
+        }
+        if(moveright){
+            this.x += this.speed;
+        }
 		ctx.drawImage(this.img, this.sx, this.sy, this.swidth, this.sheight, this.x, this.y, this.width, this.height);
 	}
+    this.drawObject = function () {
+        ctx.drawImage(this.img, this.sx, this.sy, this.swidth, this.sheight, this.x, this.y, this.width, this.height);
+    }
 }
 
 
@@ -47,10 +53,12 @@ function reset() {
 }
 
 $(document).ready(function(){
-	delay = 4000; // milliseconds
+	delay = 1000/30; // milliseconds
 	ctx = document.getElementById('game').getContext('2d');
 	initialize();
+
 	avatar.onload = function() {
+         console.log("here");
 		drawGame();
 		setInterval(drawGame, delay); // draw refers to the function
 	};
@@ -59,9 +67,15 @@ $(document).ready(function(){
 function initialize() {
 	avatar = new Image();
 	avatar.src = 'assets/images/simon.png';
-	items.avatar = new Item(avatar, 0, 50, 0, 30, 0, 50, 0, 50, 50);
+	items.avatar = new Item(avatar, 0, 0, 638, 850, 0, 0, 20, 20, 5);
+    items.objects = new Item(avatar, 0, 0, 638, 850, 900, 200, 20, 20, 5);
+
+
+    //creation array
 	
-    document.addEventListener("keydown", KeyDown, true);
+    document.addEventListener("keydown", KeyDown, false);
+    document.addEventListener("keyup", KeyUp, false);
+
     function KeyDown(evt)
     {
         var KeyID = evt.keyCode;
@@ -72,19 +86,45 @@ function initialize() {
         		break;
             case 37:
             case 65:
-                items.avatar.moveleft();
+                moveleft=true;
                 break;
             case 39:
             case 68:
-                items.avatar.moveright();
+               moveright=true;
                 break;
             case 38:
             case 87:
-                items.avatar.moveup();
+                moveup=true;
                 break;
             case 40:
             case 83:
-                items.avatar.movedown();
+                movedown=true;
+                break;
+        }
+    }
+    function KeyUp(evt)
+    {
+        var KeyID = evt.keyCode;
+        switch(KeyID)
+        {
+            case 32:
+                items.avatar.breathefire();
+                break;
+            case 37:
+            case 65:
+                moveleft=false;
+                break;
+            case 39:
+            case 68:
+                moveright=false;
+                break;
+            case 38:
+            case 87:
+                moveup=false;
+                break;
+            case 40:
+            case 83:
+                movedown=false;
                 break;
         }
     }
@@ -92,25 +132,45 @@ function initialize() {
 
 
 function drawGame() {
-	//addBackgrounds();
+	renderBackgrounds();
 	//addOverlays();
-	addAvatar();		
+	renderAvatar();	
+
+    renderObjects();	
+
+        gameLoops();
+    
 }
 
-function addBackgrounds () {
-	ctx.fillStyle = "#191970";
+function renderBackgrounds () {
+	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, 800, 400);
 }
 
 function addOverlays() {
 }
 
-function addAvatar() {
+function renderObjects(){
+    items.objects.x-=items.objects.speed;
+    $(items.objects.img).ready(function () {
+        items.objects.drawObject();
+    });
+
+
+}
+function renderAvatar() {
 	$(items.avatar.img).ready(function () {
 		items.avatar.drawme();
 	});
 }
 
+function gameLoops(){
+
+    if(collision(items.avatar,items.objects)){
+        alert("you dead");
+    }
+
+}
 function collision(obj1, obj2){
     if (obj1.x + obj1.width - 5 < obj2.x) {
       	return false;
