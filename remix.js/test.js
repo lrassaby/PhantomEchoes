5e4403d7cda9be824c36a2416feba1e331e9b803
Benjamin Mesirow <benmesirow@wr-130-64-195-36.medford.tufts.edu>
2013-10-26 03:07:43 -0400

    it works again!

--- remix.js/test.js
+++ remix.js/test.js
@@ -1,9 +1,9 @@
 var apiKey ='1DXWYOVVQHNF7AR18';
-var trackURL = 'http://localhost:8000/Red_Oyster_Cult.mp3';
+var trackURL = 'http://localhost:8000/Freestyle.mp3';
 //var trackID = 'SOJARUS1338A5D5F1C';
 var context = new webkitAudioContext();
-var _title = 'Red Oyster Cult';
-var _artist = 'Guster';
+var _title = 'Freestyle';
+var _artist = 'Taalbi Brothers';
 var url = 'http://developer.echonest.com/api/v4/song/search?format=json&bucket=audio_summary';
 $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey}, function(data) {
   var analysisURL = data.response.songs[0].audio_summary.analysis_url;
@@ -22,7 +22,7 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey}, function(data) {
         for (var i = 0; i < track.analysis.beats.length; i++) {
           bpm = 60/parseFloat(track.analysis.beats[i].duration);
           //console.log(track.analysis.beats.length);
-            //console.log("next");
+          //console.log("next");
             //console.log(track.analysis.beats[i].time);
           console.log(bpm);
           bs.push(bpm);
@@ -30,9 +30,10 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey}, function(data) {
           volume.push(loud);
           game_beats.push({"bpm": bpm, "loudness": loud});
           //console.log(track.analysis.beats[i].oseg.loudness_start);
-            //console.log('next');
+          //console.log('next');
         }
-        console.log("random string");
+
+        //console.log("random string");
         console.log(volume);
         bs.sort();
           //var v = volume.copy();
@@ -44,7 +45,7 @@ $.getJSON(url, {title:_title, artist:_artist, api_key:apiKey}, function(data) {
         console.log(game_beats);
         console.log("Min volume: " + Math.min.apply(null, volume));
         console.log("Max volume: " + Math.max.apply(null, volume));
-        console.log("BLAH");
+        //console.log("BLAH");
       } 
     });
 });
